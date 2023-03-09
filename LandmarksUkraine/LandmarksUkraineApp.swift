c3c661079dd83a961ed5bfa9ba00ae68210283a0
Oleh Titov <oleg.titov81@gmail.com>
2023-03-09 22:19:23 +0100

    Initial Commit

---
+++ LandmarksUkraine/LandmarksUkraineApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksUkraineApp.swift
+//  LandmarksUkraine
+//
+//  Created by Oleh Titov on 09.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksUkraineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
